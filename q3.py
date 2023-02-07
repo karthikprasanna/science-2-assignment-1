b94b52c7654a8245192dcde0f708ff545daf2895
Karthik Prasanna N <karthikprasanna@pop-os.localdomain>
2023-02-07 10:04:43 +0530

    demossed q3

--- q3.py
+++ q3.py
@@ -1,14 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_eigenvalues(N, D_values):
+    for D in D_values:
+        muo = 0
+        sigma_square = 1
+        M = np.random.normal(muo, sigma_square, (N, N))
+        D = -D
+        np.fill_diagonal(M, D)
+        eigenvalues = np.linalg.eigvals(M)
+        real_eigenvalues = np.real(eigenvalues)
+        imag_eigenvalues = np.imag(eigenvalues)
+        plt.scatter(real_eigenvalues, imag_eigenvalues, label=f'D={D}')
+
 N = 500
 D_values = [0, 1, 5, 10]
-
-for D in D_values:
-    M = np.random.normal(0, 1, (N, N))
-    np.fill_diagonal(M, -D)
-    eigenvalues = np.linalg.eigvals(M)
-    plt.scatter(np.real(eigenvalues), np.imag(eigenvalues), label=f'D={D}')
+plot_eigenvalues(N, D_values)
 
 plt.xlabel('Real(λ)')
 plt.ylabel('Imag(λ)')
